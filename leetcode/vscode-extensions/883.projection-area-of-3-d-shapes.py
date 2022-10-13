9458acde668c38f66c9bbd874e1410f1cb8c4fb1
Lootmann <ryota.tsugawa.sites.tabs@gmail.com>
2022-10-14 05:12:10 +0900

    leetcode easy - not finished D: #4

---
+++ leetcode/vscode-extensions/883.projection-area-of-3-d-shapes.py
@@ -0,0 +1,14 @@
+#
+# @lc app=leetcode id=883 lang=python3
+#
+# [883] Projection Area of 3D Shapes
+#
+from typing import List
+
+# @lc code=start
+class Solution:
+    def projectionArea(self, grid: List[List[int]]) -> int:
+        return 0
+
+
+# @lc code=end
